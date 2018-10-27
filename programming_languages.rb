39ed634132f026f6d60b3cccd881dcf7be195eef
Meghan Campbell <mlc03e@gmail.com>
2018-10-27 00:18:51 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,6 @@ require "pry"
 def reformat_languages(languages)
  new_hash = {} 
  languages[:ruby] = {:type => "interpreted", :style => [:oo]}
- binding.pry
  puts new_hash << languages
+ binding.pry
 end
